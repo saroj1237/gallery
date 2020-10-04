9843115c247577582fb656481d7b60c11d54b2cf
Saroj Basnet <54398451+saroj-sudo@users.noreply.github.com>
2020-10-04 23:38:04 +0545

    auth2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,32 @@
-import React from "react";
-import { BrowserRouter as Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Link, useHistory } from "react-router-dom";
+import firebase from "../config/firebase";
 
 function Header() {
+  const [isLoggeIn, setisLoggeIn] = useState(false);
+  const history = useHistory();
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setisLoggeIn(true);
+      }
+      console.log(user);
+    });
+  }, []);
+
+  function handleLogout() {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        setisLoggeIn(false);
+        history.replace("/login");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
   return (
     <nav className=" py-4 border bg-gray-900 text-white">
       <ul className="flex justify-between text-2xl px-10">
@@ -14,7 +39,11 @@ function Header() {
           </li>
         </span>
         <li>
+          {isLoggeIn ? (
+            <button onClick={handleLogout}>Logout</button>
+          ) : (
             <Link to="/login">Login</Link>
+          )}
         </li>
       </ul>
     </nav>
--- src/page/Login.js
+++ src/page/Login.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import firebase from "../config/firebase";
 
 function Login() {
+  const [isLoading, setisLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [form, setform] = useState({ email: "", password: "" });
+  // const [isLoggedIn, setisLoggedIn] = useState(false)
+  const history = useHistory();
+
   function handleForm(e) {
+    if (isLoading) return;
+    setisLoading(true);
     e.preventDefault();
     firebase
       .auth()
-      .signInWithEmailAndPassword("demotesting321@g.com", "demotesting321");
+      .signInWithEmailAndPassword(form.email, form.password)
+      .then((res) => {
+        setError("");
+        setisLoading(false);
+        history.replace("/");
+        // setisLoggedIn(true)
+      })
+      .catch((e) => {
+        setError(e.message);
+        setisLoading(false);
+      });
   }
+
+  function handleInput(e) {
+    setform({ ...form, [e.target.name]: e.target.value });
+  }
+
+  // if(isLoggedIn) return <Redirect to="/"/>
 
   return (
     <div className="flex h-screen bg-yellow-500">
@@ -15,39 +40,37 @@ function Login() {
         className="bg-gray-600 p-10 rounded-lg m-auto w-full max-w-sm"
         onSubmit={handleForm}
       >
+        {error}
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/3">
-            <label
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              for="inline-full-name"
-            >
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
               Email
             </label>
           </div>
           <div className="md:w-2/3">
             <input
               className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-              id="inline-full-name"
+              name="email"
               type="text"
-              value="Jane Doe"
+              value={form.email}
+              onChange={handleInput}
             />
           </div>
         </div>
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/3">
-            <label
-              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              for="inline-password"
-            >
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
               Password
             </label>
           </div>
           <div className="md:w-2/3">
             <input
               className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-              id="inline-password"
+              name="password"
               type="password"
               placeholder="******************"
+              value={form.password}
+              onChange={handleInput}
             />
           </div>
         </div>
@@ -65,7 +88,11 @@ function Login() {
               className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
               type="submit"
             >
-              Sign Up
+              {isLoading ? (
+                <i className="fas fa-circle-notch fa-spin "></i>
+              ) : (
+                "Log In"
+              )}
             </button>
           </div>
         </div>
